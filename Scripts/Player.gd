0a32fba63b68cb672226b5c6112a69c9641e147b
clepfeed <clepfeed@yandex.by>
2024-06-27 15:13:00 +0300

    add camera and teleportation to other scenes

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -57,5 +57,4 @@ func _process(delta):
 		$AnimatedSprite2D.stop()
 	
 	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size)
 	pass
---
+++ Scripts/Teleport.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+@export var where_to_go = "Location_2"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_area_entered(area):
+	var newScene = "res://" + where_to_go + ".tscn"
+	get_tree().change_scene_to_file(newScene)
+	pass # Replace with function body.
